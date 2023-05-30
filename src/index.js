afd9c5f9625accf178ed73b9271f2ded43611905
MelissaStiven <stivenhermite@gmail.com>
2023-05-29 23:08:17 -0400

    add Priority Colors

--- src/index.js
+++ src/index.js
@@ -2,20 +2,25 @@ document.addEventListener("DOMContentLoaded", () => {
   addingEventListeners()
 
 });
+
+
 function addingEventListeners() {
   document
   .getElementById("create-task-form")
   .addEventListener("submit", handleFormSubmit)
 }
+
+
 function handleFormSubmit(e) {
   e.preventDefault()
   const task = e.target [0].value
+  const priorityLevel = parseInt(e.target.priority.value)
   
 
-  displayTask(task)
+  displayTask(task, priorityLevel)
 }
 
-function displayTask(task) {
+function displayTask(task, priorityLevel) {
 
 const taskUl = document.getElementById("tasks")
 
@@ -27,11 +32,22 @@ deleteBtn.addEventListener("click", deleteTask)
 
 
 taskLi.textContent = task + " "
+taskLi.style.color = getPriorityColor(priorityLevel)
 taskLi.appendChild(deleteBtn)
 
 taskUl.appendChild(taskLi)
 }
  function deleteTask(e) {
   e.target.parentNode.remove()
-
+ }
+ function getPriorityColor(priorityLevel) {
+  if (priorityLevel === 1) {
+    return "red"
+  }
+  else if (priorityLevel === 2) {
+    return "blue"
+  }
+  else {
+    return "green"
+  }
  }
